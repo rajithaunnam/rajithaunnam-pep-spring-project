d21183c720d6520c98ae1e8e0416b1068ebe0e38
rajithaunnam <rajithaunnam97@gmail.com>
2024-08-08 11:51:52 +0530

    done

--- src/main/java/com/example/SocialMediaApp.java
+++ src/main/java/com/example/SocialMediaApp.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.client.RestTemplate;
 
 import static org.springframework.boot.SpringApplication.run;
 
